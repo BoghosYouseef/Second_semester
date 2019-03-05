5c88e7a677e015a92409841b7d79283500e41d2c
Boghos Youseef <43525406+BoghosYouseef@users.noreply.github.com>
2019-03-05 05:58:05 +0100

    First program in C
    a problem with the calculator

---
+++ Learning_C.cpp
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+//	char NameOfCharacter[20];
+//	int CharacterAge;
+//	printf("Enter Your name: \n");
+//	scanf("%s",NameOfCharacter);
+//	printf("Enter Your age: \n");
+//	scanf("%d",&CharacterAge);
+//	printf("Hello %s!\n", NameOfCharacter );
+//	printf("Hello Johnathan!\n");
+//	printf("How old are you now,%s?\n", NameOfCharacter);
+//	printf("I have just turned %d !\n", CharacterAge);
+//	printf("%f\n",0.11+0.89);
+//	printf("%f\n", pow(2, 3));
+//	printf("%f\n", sqrt(8));
+//	float SquareRootOfEight = sqrt(8);
+//	printf("%f\n",SquareRootOfEight);
+//	printf("%f\n", ceil(SquareRootOfEight));
+//	printf("%f\n", floor(SquareRootOfEight));
+//	printf("This is the correctly rounded square root of 8: %f\n", round(SquareRootOfEight));
+
+//	*************************************
+//  *************************************
+//* Calculator(calculates using the four basic operations of two numbers that the user provides)
+//  ERROR with last 3 printf() statements
+//	double num1;
+//	double num2;
+//	printf("give me a number: ");
+//	scanf("%lf",&num1);
+//	printf("give me a second number: ");
+//	scanf("%lf",&num2);
+//	printf("The sum of those numbers is equal to: %f/n",num1+num2);
+//	printf("The subtraction of of the first number from the second is equal to: %f/n",num1-num2);
+//	printf("The multilication of those numbers is equal to: %f/n",num1+num2);
+//	printf("The division of the first over the second is equal to: %f/n", num1/num2);
+//	*************************************
+//  *************************************
+
+//	*************************************
+//  *************************************
+//* MADLIBS game "The Three Pigs" (Prompts the user of ajdectives, nouns etc... and creates a madlib)
+//	char Adjective1[20];
+//	char Adjective2[20];
+//	char Adjective3[20];
+//	char Adjective4[20];
+//	char Thing1[20];
+//	char Thing2[20];
+//	char Thing3[20];
+//	char PersonThingAnimal[20];
+//	char place1[20];
+//	char place2[20];
+//	char place3[20];
+//	char place4[20];
+//	char interjection1[20];
+//	char interjection2[20];
+//	char interjection3[20];
+//	char Liquid[20];
+//
+//	printf("Enter an Adjective:");
+//	scanf("%s", Adjective1);
+//	printf("Enter an Thing:");
+//	scanf("%s",Thing1);
+//	printf("Enter an Thing:");
+//	scanf("%s",Thing2);
+//	printf("Enter an Thing:");
+//	scanf("%s",Thing3);
+//	printf("Enter a person, thing or animal:");
+//	scanf("%s",PersonThingAnimal);
+//	printf("Enter a place:");
+//	scanf("%s",place1);
+//	printf("Enter an Adjective:");
+//	scanf("%s", Adjective2);
+//	printf("Enter an Interjection (single word you yell):");
+//	scanf("%s", interjection1);
+//	printf("Enter a place:");
+//	scanf("%s", place2);
+//	printf("Enter an Adjective:");
+//	scanf("%s", Adjective3);
+//	printf("Enter an Interjection (single word you yell):");
+//	scanf("%s", interjection2);
+//	printf("Enter a place:");
+//	scanf("%s", place3);
+//	printf("Enter an Adjective:");
+//	scanf("%s", Adjective4);
+//	printf("Enter an Interjection (single word you yell):");
+//	scanf("%s", interjection3);
+//	printf("Enter a Liquid:");
+//	scanf("%s", Liquid);
+//	printf("Enter a place:");
+//	scanf("%s", place4);
+//	printf("Once upon a time, there were three %s pigs", Adjective1);
+//	printf("The first pig built his house out of %s", Thing1);
+//	printf("The second pig built his house out of %s", Thing2);
+//	printf("The third pig built his house out of %s ", Thing3);
+//	printf("One day a wolf went looking for his %s ", PersonThingAnimal);
+//	printf("He thought the three pigs took his %s ", PersonThingAnimal);
+//	printf("WOLF:Those pigs took my %s /n", PersonThingAnimal);
+//	printf("Narrator: He went to the first pig's house in %s ", place1);
+//	printf("PIG 1: Welcome to %s ", place1);
+//	printf("The wolf knocked on the door and said(WOLF):%1$s pig %1$s pig let me in!", Adjective2);
+//	printf("PIG 1: Not by the hair of my chinny, chin, chin. ");
+//	printf("NARRATOR: The wolf got mad and said(WOLF): %s ", interjection1);
+//	printf("NARRATOR: And he huffed and puffed and blew the house down. The first pig ran to the second pig's house in %s ", place2);
+//	printf("PIG 2: Welcome to %s ", place2);
+//	printf("NARRATOR:The wolf knocked on the door and said...(WOLF):%1$s pig... %1$s pig.Let me in!",Adjective3);
+//	printf("PIG 2: Not by the hair of my chinny, chin, chin.");
+//	printf("NARRATOR: The wolf got mad and said:");
+//	printf("(WOLF): %s ", interjection2);
+//	printf("NARRATOR: And he huffed and puffed and blew the house down.The pigs ran to the third pig's house in %s ", place3);
+//	printf("PIG 3:  Welcome to %s ", place3);
+//	printf("ARRATOR: The wolf knocked on the door and said:(WOLF):%1$s pig... %1$s pig.  Let me in! ", Adjective4);
+//	printf("PIG 3: Not by the hair of my chinny, chin, chin. ");  
+//	printf("NARRATOR:The wolf got mad and said:(WOLF):%s ", interjection3);
+//	printf("NARRATOR:And he huffed and puffed and huffed and puffed and huffed and pulled... but the house didn't fall down.  The wolf decides to go down the chimney.  The pigs hurried to the fireplace, made a fire and boiled some %s ", Liquid);
+//	printf("NARRATOR:wolf came down the chimney and fell in the boiling %s ", Liquid);
+//	printf("burned his tail and ran all the way home to %s ", place4);
+//	printf("THE END");
+	return 0;
+}
